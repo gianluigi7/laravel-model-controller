871eb091c673b6983e85fc1ed48db95195e39a2f
gianluigi7 <gianluigiferraioli7@gmail.com>
2024-07-08 19:01:51 +0200

    scss

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
     public function index() {
-        return view('welcome');
+
+        $movies = Movie::all();
+
+        $data = [
+            'movies' => Movie::all()
+        ];
+
+        return view('welcome', $data);
+                               //oppure compact('movies')
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,8 +8,21 @@
     @vite('resources/js/app.js')
 </head>
 <body>
-    <h1>template Laravel</h1>
-    <h2>prova</h2>
+    <h1>Movies</h1>
+    <div class="container">
+
+        <ul>
+            @foreach ($movies as $movie)
+            <li>
+                <p>{{ $movie -> title }}</p>
+                <p>titolo originale :{{ $movie -> original_title }}</p>
+                <p>provenienza: {{ $movie -> nationality }}</p>
+                <p>uscita :{{ $movie -> date }}</p>
+                <p> {{ $movie -> vote }}</p>
+            </li>
+            @endforeach
+        </ul>
+    </div>
 
 <!-- ESEMPIO IMG
 <img src="{{ Vite::asset('resources/img/logo.png') }}" alt=""> -->
